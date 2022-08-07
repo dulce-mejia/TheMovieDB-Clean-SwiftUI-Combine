f32ce40d3a2b6b85046aba01768c57b7296e8d2a
Dulce Mejia <dulce@sicar.mx>
2022-08-07 10:07:29 -0500

    initial commit

---
+++ TheMovieDB+Clean+SwiftUI+Combine/Core/TheMovieDB_Clean_SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDB_Clean_SwiftUI_CombineApp.swift
+//  TheMovieDB+Clean+SwiftUI+Combine
+//
+//  Created by Gardenia Mejia on 07/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDB_Clean_SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FeedView()
+        }
+    }
+}
---
+++ TheMovieDB+Clean+SwiftUI+Combine/UI/Feed/FeedView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TheMovieDB+Clean+SwiftUI+Combine
+//
+//  Created by Gardenia Mejia on 07/08/22.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
