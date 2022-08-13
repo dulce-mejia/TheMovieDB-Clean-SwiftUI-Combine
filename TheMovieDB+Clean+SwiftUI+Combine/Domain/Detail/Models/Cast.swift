f086b3c794d03d52cdad817286fab992c2ee2f61
Gardenia Mejia <dgma0607@gmail.com>
2022-08-12 20:03:55 -0500

    added models needed in Detail Module

---
+++ TheMovieDB+Clean+SwiftUI+Combine/Domain/Detail/Models/Cast.swift
@@ -0,0 +1,13 @@
+//
+//  Cast.swift
+//  TheMovieDB+Clean+SwiftUI+Combine
+//
+//  Created by Gardenia Mejia on 12/08/22.
+//
+
+import Foundation
+
+public struct Cast {
+    var name: String
+    var profilePath: String?
+}
